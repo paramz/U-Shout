668ce76cd44a9e7013b2a96c1ebb2d2208d3cbf3
Xingchen Hong <hello@xc-h.net>
2013-11-24 16:40:03 -0600

    ver 2013.11.22.0

--- chrome_plugin/js/main.js
+++ chrome_plugin/js/main.js
@@ -1,7 +1,8 @@
 // this wrapper ensures script integrity
 (function (window, document, undefined) {
 	// this jQuery wrapper ensures that the function gets executed right after the DOM is ready
-	$(document).ready(function () { console.log('ready'); });
+	$(document).ready(function () { console.log('dom ready'); });
+	
 	$(function () {
 		ushout($(document.body));
 	});
