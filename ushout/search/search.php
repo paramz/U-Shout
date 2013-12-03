45faf7ef756d0edeceb85b5fa792d2f454b6ed94
Krish Jadhwani <krishjadhwani@gmail.com>
2013-12-02 20:15:15 -0600

    updated search.php

--- ushout/search/search.php
+++ ushout/search/search.php
@@ -1,8 +1,10 @@
-<?php
+<?php include '../template-header.php';?>
+	<?php
 
-if ($_SERVER['REQUEST_METHOD'] == 'GET')
-{
-      include('search.php.view');
-}
+	if ($_SERVER['REQUEST_METHOD'] == 'GET')
+	{
+		  include('search.php.view');
+	}
 
-?>
+	?>
+<?php include '../template-footer.php';?>
