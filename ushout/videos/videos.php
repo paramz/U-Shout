038c9f4930fa47ef5814cd583aebab1bcef5c9a4
Ben DuPont <ben@nebland.com>
2013-12-04 00:27:34 -0600

    update search string on video page

--- ushout/videos/videos.php
+++ ushout/videos/videos.php
@@ -5,7 +5,7 @@ require ('video_model.php');
 if ($_SERVER['REQUEST_METHOD'] == 'GET')
 {
       if (!isset($_GET["search"]))
-          $search_text = "videos ...";
+          $search_text = "filter videos ...";
       else
           $search_text = $_GET["search"];
       
